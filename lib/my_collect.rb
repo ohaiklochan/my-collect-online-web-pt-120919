65ad95f9283268bc7cf1a083be99c0c1c99c7031
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 23:38:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,5 +2,9 @@ def my_collect(array)
   i = 0 
     name = []
     while i < array.length
-    name << yield(array[i])
-    i += 1
+      name << yield(array[i])
+      i += 1
+    end
+    name
+  end
+  
